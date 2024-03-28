45b3df0f6de670b4d42974afe40a907b1c794cd3
Plamen Chichkov <plamenggg233@gmail.com>
2024-03-28 12:34:27 +0200

    ControllerScreen
    change the ControllerScreen from class to functional components.
    NOT TESTED !!!

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import './App.css';
 
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ControllerScreen from './components/ControllerScreen';
-
-
 
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Controllers from './components/Controllers';
+import ControllersScreen from './components/ControllersScreen';
 
 
 function App() {  
@@ -20,7 +18,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={ () => <Home />} />
             <Route path="/controllers" component={() => <Controllers />} />
-            <Route path="/controller/:id" component={() => <ControllerScreen  />} />
+            <Route path="/controller/:id" component={() => <ControllersScreen  />} />
           </Switch>
         </div>
       </Container>
