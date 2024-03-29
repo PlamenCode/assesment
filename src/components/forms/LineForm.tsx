a4f7dc26ae3709e4045dfed38d39000e29c3849e
Plamen Chichkov <plamenggg233@gmail.com>
2024-03-29 11:54:02 +0200

    LineForm and SignalProgram Form changed from class to functional components. NOT TESTED !!!

--- src/components/forms/LineForm.tsx
+++ src/components/forms/LineForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import ILine from '../../interfaces/ILine';
 import TracApi from '../../services/trac-api';
@@ -18,86 +18,82 @@ type FormProps = {
 	heading?: number,
 	existingLines?: number[],
 	save: () => void
-}
+};
 
-export default class LineForm extends React.Component<FormProps, FormState> {
-	state: FormState = {
-		heading: this.props.heading,
-		number: this.props.data?.number,
-		saturation: this.props.data?.saturation
-	};
+export default function LineForm( props: FormProps) {
+    const [state, setState] = useState<FormState>({
+		heading: props.heading,
+		number: props.data?.number,
+		saturation: props.data?.saturation
+	});
 
-	handleSubmit = async (e: any) => {
-		e.preventDefault();
-		if (!this.state.number) {
-			this.setState({ error: 'Line not specified' })
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+		if (!state.number) {
+			setState({ ...state, error: 'Line not specified' })
 			return;
 		}
 
-		if (this.props.existingLines && this.props.existingLines.indexOf(this.state.number) >= 0) {
-			this.setState({ error: 'Line already exists' })
+		if (props.existingLines && props.existingLines.indexOf(state.number) >= 0) {
+			setState({ ...state, error: 'Line already exists' })
 		}
 
-		this.setState({ error: undefined })
+		setState({ error: undefined })
 		const model = {
-			controller: this.props.controller,
-			heading: this.state.heading,
-			number: this.state.number,
-			saturation: this.state.saturation
+			controller: props.controller,
+			heading: state.heading,
+			number: state.number,
+			saturation: state.saturation
 		}
-		if (!!this.props.data) {
+		if (!!props.data) {
 			await Api.editLine(model);
 		} else {
 			await Api.addLine(model);
 		}
 
-		this.props.save();
-	}
-
-	render() {
-		return (
-			<Modal
-				show={true}
-				onHide={() => this.props.save()}
-				backdrop="static"
-				keyboard={true}>
-				<Modal.Header>
-					<Modal.Title>{this.props.data ? 'Edit' : 'Add new'} line {this.props.heading && ` for heading ${this.props.heading}`}</Modal.Title>
-				</Modal.Header>
-				<Form onSubmit={this.handleSubmit}>
-					<Modal.Body>
-						{!this.props.heading &&
-							<Form.Group controlId='heading'>
-								<Form.Label>Heading</Form.Label>
-								<Form.Control as='input' type='number'
-									onChange={async e => this.setState({ heading: +e.target.value })} >
-								</Form.Control>
-							</Form.Group>
-						}
-						<Form.Group controlId='line'>
-							<Form.Label>Line</Form.Label>
-							<Form.Control type='number' disabled={!!this.props.data} min='1'
-								onChange={e => this.setState({ number: +e.target.value })}
-								value={this.state.number}
-								isInvalid={!!this.state.error} />
-							<Form.Control.Feedback type='invalid'>
-								{this.state.error}
-							</Form.Control.Feedback>
-						</Form.Group>
-						<Form.Group controlId='saturation'>
-							<Form.Label>Saturation</Form.Label>
-							<Form.Control type='number' step='0.1'
-								onChange={e => this.setState({ saturation: +e.target.value })}
-								value={this.state.saturation} />
-						</Form.Group>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant="secondary" onClick={() => this.props.save()}>Cancel</Button>
-						<Button type="submit">{!this.props.data ? 'Add' : 'Edit'}</Button>
-					</Modal.Footer>
-				</Form>
-			</Modal>
-		)
-	}
-
+		props.save();
+    };
+  return (
+    <Modal
+        show={true}
+        onHide={() => props.save()}
+        backdrop="static"
+        keyboard={true}>
+        <Modal.Header>
+            <Modal.Title>{props.data ? 'Edit' : 'Add new'} line {props.heading && ` for heading ${props.heading}`}</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={handleSubmit}>
+            <Modal.Body>
+                {!props.heading &&
+                    <Form.Group controlId='heading'>
+                        <Form.Label>Heading</Form.Label>
+                        <Form.Control as='input' type='number'
+                            onChange={async e => setState({ ...state, heading: +e.target.value })} >
+                        </Form.Control>
+                    </Form.Group>
+                }
+                <Form.Group controlId='line'>
+                    <Form.Label>Line</Form.Label>
+                    <Form.Control type='number' disabled={!!props.data} min='1'
+                        onChange={e => setState({ ...state, number: +e.target.value })}
+                        value={state.number}
+                        isInvalid={!!state.error} />
+                    <Form.Control.Feedback type='invalid'>
+                        {state.error}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId='saturation'>
+                    <Form.Label>Saturation</Form.Label>
+                    <Form.Control type='number' step='0.1'
+                        onChange={e => setState({ ...state, saturation: +e.target.value })}
+                        value={state.saturation} />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => props.save()}>Cancel</Button>
+                <Button type="submit">{!props.data ? 'Add' : 'Edit'}</Button>
+            </Modal.Footer>
+        </Form>
+    </Modal>
+)
 }
--- src/components/forms/SignalProgramForm.tsx
+++ src/components/forms/SignalProgramForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Modal, Form } from 'react-bootstrap';
 import ISignalProgram from '../../interfaces/ISignalProgram';
 import TracApi from '../../services/trac-api';
@@ -19,81 +19,79 @@ type FormProps = {
     save: () => void
 }
 
-export default class SignalProgramForm extends React.Component<FormProps, FormState> {
-    state: FormState = {
-        number: this.props.data?.number,
-        greenLength: this.props.data?.greenLength || 60
-    }
+export default function SignalProgramForm(props: FormProps) {
+    const [state, setState] = useState<FormState>({
+        number: props.data?.number,
+        greenLength: props.data?.greenLength || 60
+    });
 
-    handleSubmit = async (e: any) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
-        if (!this.state.number) {
-            this.setState({ error: 'No Signal program' })
+        if (!state.number) {
+            setState({ ...state, error: 'No Signal program' })
             return;
         }
 
-        if (this.props.existingSps && this.props.existingSps.indexOf(this.state.number) >= 0) {
-            this.setState({ error: 'Signal program already exists' });
+        if (props.existingSps && props.existingSps.indexOf(state.number) >= 0) {
+            setState({ ...state, error: 'Signal program already exists' });
             return;
         }
 
-        this.setState({ error: undefined })
+        setState({ ...state, error: undefined })
         const model = {
-            controller: +this.props.controller,
-            heading: this.props.heading,
-            number: this.state.number,
-            greenLength: this.state.greenLength
+            controller: + props.controller,
+            heading: props.heading,
+            number: state.number,
+            greenLength: state.greenLength
         }
-        if (!!this.props.data) {
+        if (!!props.data) {
             await Api.editSp(model);
         } else {
             await Api.addSp(model);
         }
 
-        this.props.save();
-    }
+        props.save();
+    };
 
-    render() {
-        return (
-            <Modal
-                show={true}
-                onHide={() => this.props.save()}
-                backdrop="static"
-                keyboard={true}>
-                <Modal.Header>
-                    <Modal.Title>{this.props.data ? 'Edit' : 'Add new'} signal program {this.props.heading && ` for heading ${this.props.heading}`}</Modal.Title>
-                </Modal.Header>
-                <Form onSubmit={this.handleSubmit}>
-                    <Modal.Body>
-                        <Form.Group controlId='sp'>
-                            <Form.Label>Signal program</Form.Label>
-                            <Form.Control type='number' disabled={!!this.props.data} min='1' max='8'
-                                onChange={e => this.setState({ number: +e.target.value })}
-                                value={this.state.number}
-                                isInvalid={!!this.state.error} />
-                            <Form.Control.Feedback type='invalid'>
-                                {this.state.error}
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                        {/* <Form.Group controlId='weight'>
-              <Form.Label>Weight</Form.Label>
-              <Form.Control type='number' min='0'
-                onChange={e => this.setState({ weight: +e.target.value })}
-                value={this.state.weight} />
-            </Form.Group> */}
-                        <Form.Group controlId='greenLength'>
-                            <Form.Label>Green Length - seconds</Form.Label>
-                            <Form.Control type='number' min='8' max='120' defaultValue='60' step='1'
-                                onChange={e => this.setState({ greenLength: +e.target.value })}
-                                value={this.state.greenLength} />
-                        </Form.Group>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => this.props.save()}>Cancel</Button>
-                        <Button type="submit">{!this.props.data ? 'Add' : 'Edit'}</Button>
-                    </Modal.Footer>
-                </Form>
-            </Modal>
-        )
-    }
+    return (
+        <Modal
+            show={true}
+            onHide={() => props.save()}
+            backdrop="static"
+            keyboard={true}>
+            <Modal.Header>
+                <Modal.Title>{props.data ? 'Edit' : 'Add new'} signal program {props.heading && ` for heading ${props.heading}`}</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Body>
+                    <Form.Group controlId='sp'>
+                        <Form.Label>Signal program</Form.Label>
+                        <Form.Control type='number' disabled={!!props.data} min='1' max='8'
+                            onChange={e => setState({ number: +e.target.value })}
+                            value={state.number}
+                            isInvalid={!!state.error} />
+                        <Form.Control.Feedback type='invalid'>
+                            {state.error}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    {/* <Form.Group controlId='weight'>
+          <Form.Label>Weight</Form.Label>
+          <Form.Control type='number' min='0'
+            onChange={e => this.setState({ weight: +e.target.value })}
+            value={this.state.weight} />
+        </Form.Group> */}
+                    <Form.Group controlId='greenLength'>
+                        <Form.Label>Green Length - seconds</Form.Label>
+                        <Form.Control type='number' min='8' max='120' defaultValue='60' step='1'
+                            onChange={e => setState({ ...state, greenLength: +e.target.value })}
+                            value={state.greenLength} />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => props.save()}>Cancel</Button>
+                    <Button type="submit">{!props.data ? 'Add' : 'Edit'}</Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    )
 }
